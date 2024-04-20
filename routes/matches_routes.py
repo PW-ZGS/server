ea2c3960de095102e25c90dbf642488f885351ca
damiano <macmacroll@gmail.com>
2024-04-20 22:28:02 +0200

    Mocked matches_routes

--- routes/matches_routes.py
+++ routes/matches_routes.py
@@ -2,6 +2,11 @@ from typing import List
 
 from fastapi import APIRouter
 from pydantic import BaseModel
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from common.credentials import POSTGRE_CONNECTION
+from common.models import RouteMatchEntity
 
 match_routes = APIRouter(
     tags=["matches"]
@@ -14,7 +19,40 @@ class Match(BaseModel):
     name: str
 
 
+def convert_route_match_to_match(route_match: RouteMatchEntity) -> Match:
+    passenger_route = route_match.passenger_route
+    driver_route = route_match.driver_route
+
+    contact = passenger_route.contact
+    name = passenger_route.user.name
+
+    return Match(
+        passengerRouteId=route_match.passenger_route_id,
+        contact=contact,
+        name=name,
+    )
+
+
 @match_routes.get("/matches", response_model=List[Match])
-async def get_matches_by_route(passenger_route_id: str):
-    matches = []
-    return matches
+def get_matches_by_route(passengerRouteId: str):
+
+    return [
+        Match(passengerRouteId='nvdivnw3u', contact='ajajaj@gmail.com', name='Simon'),
+        Match(passengerRouteId='ndsadvdivnw3u', contact='ajajaj2@gmail.com', name='Simon2')
+    ]
+
+    engine = create_engine(POSTGRE_CONNECTION)
+    sesion_maker = sessionmaker(engine)
+    session = sesion_maker()
+
+    try:
+        route_matches = session.query(RouteMatchEntity).filter(
+            RouteMatchEntity.passenger_route_id == passengerRouteId
+        ).all()
+
+        matches = [convert_route_match_to_match(route_match) for route_match in route_matches]
+
+        return matches
+
+    finally:
+        session.close()
